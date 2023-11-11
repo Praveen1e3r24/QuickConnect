d712e851485d7dc074e6263e60a1e099baa403d3
Vetriselvan Praveen <116857991+Praveen1e3r24@users.noreply.github.com>
2023-11-11 15:55:36 +0800

    Merging wit jq for authentication and db 2.0  11/11/2023

--- app/src/main/java/com/example/quickconnect/User.java
+++ app/src/main/java/com/example/quickconnect/User.java
@@ -1,26 +1,40 @@
 package com.example.quickconnect;
-
 public class User {
 
-      String usertype,FirstName,LastName,password,username;
+    private String userId;
+    private String email;
+    private String FirstName;
+    private String LastName;
+
+    private String fullName;
+
+    private String userType;
 
     public User() {
     }
 
-    public User(String usertype, String firstName, String lastName, String password, String username) {
-        this.usertype = usertype;
+    public User(String userId, String email, String firstName, String lastName) {
+        this.userId = userId;
+        this.email = email;
         FirstName = firstName;
         LastName = lastName;
-        this.password = password;
-        this.username = username;
+        fullName = firstName + " " + lastName;
     }
 
-    public String getUsertype() {
-        return usertype;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setUsertype(String usertype) {
-        this.usertype = usertype;
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getFirstName() {
@@ -39,20 +53,19 @@ public class User {
         LastName = lastName;
     }
 
-    public String getPassword() {
-        return password;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUserType() {
+        return userType;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public void setUserType(String userType) {
+        this.userType = userType;
 }
-
+}
